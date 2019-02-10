dc47aa40cda86802d2644822baa55a6aa27bc155
Emilbekov Nurdin Emilbekovich <nurdin.emilbek@gmail.com>
2019-02-10 23:35:54 +0600

    first commit

---
+++ chess/1064.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8')
+let lines = input.split('\n')
+
+count=0
+sum=0.0
+let i = parseInt(lines[0])
+    if  (range(1,7)) {
+        n = float(input())
+} else ( n > 0) 
+        sum = sum + n
+        count=count+1
+
+
+print("{} valores positivos".format(count))
+print("%0.1f"%(sum/count))
